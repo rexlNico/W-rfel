13e2e5a2c70decf73a0e3a52cbfa67a386b972d5
rexlNico <31076566+rexlNico@users.noreply.github.com>
2019-05-21 14:32:29 +0200

    Update and rename W_rfel.ino to Würfel.ino

---
+++ Würfel.ino
@@ -0,0 +1,63 @@
+boolean used = true;
+
+void setup() {
+	for (int i = 0; i < 6; i++) {
+		pinMode(i, OUTPUT);
+	}
+	pinMode(7, INPUT);
+}
+
+void loop() {
+	if (digitalRead(7) == HIGH) {
+		if (used == true) {
+			used = false;
+			int r = random(1, 6);
+			light(r)
+			delay(3000)
+			used = true;
+		}
+	}
+}
+
+void light(int zahl) {
+	for (int i = 0; i < 7; i++) {
+		digitalWrite(i, 0);
+	}
+	switch (zahl) {
+	case 1:
+		digitalWrite(3, 1);
+		break;
+	case 2:
+		digitalWrite(2, 1);
+		digitalWrite(4, 1);
+		break;
+	case 3:
+		digitalWrite(2, 1);
+		digitalWrite(3, 1);
+		digitalWrite(4, 1);
+		break;
+	case 4:
+		digitalWrite(0, 1);
+		digitalWrite(2, 1);
+		digitalWrite(4, 1);
+		digitalWrite(6, 1);
+		break;
+	case 5:
+		digitalWrite(0, 1);
+		digitalWrite(2, 1);
+		digitalWrite(3, 1);
+		digitalWrite(4, 1);
+		digitalWrite(6, 1);
+		break;
+	case 6:
+		digitalWrite(0, 1);
+		digitalWrite(1, 1);
+		digitalWrite(2, 1);
+		digitalWrite(4, 1);
+		digitalWrite(5, 1);
+		digitalWrite(6, 1);
+		break;
+	default:
+		break;
+	}
+}
